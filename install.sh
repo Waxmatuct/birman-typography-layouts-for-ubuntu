5bd4cdaa6a1a999fee5a09c870bc023b55697802
Waxmatuct <49368556+Waxmatuct@users.noreply.github.com>
2021-03-18 07:13:35 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,21 +21,3 @@ sudo "$BASE/helper/xmladd.py" /usr/share/X11/xkb/rules/evdev.xml "$BASE/rules/va
 sudo rm /usr/share/X11/xkb/rules/evdev.xml
 sudo mv /tmp/evdev.xml /usr/share/X11/xkb/rules/evdev.xml
 
-# Enable AltGr
-gsettings set org.gnome.desktop.input-sources xkb-options "['lv3:ralt_switch']"
-
-# Enable keyboard layouts
-gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us+typo-birman-en'), ('xkb', 'ru+typo-birman-ru')]"
-
-# Show further instructions
-echo -e "\e[32mDone! Please log out and log in again to activate the new keyboard layouts.\e[0m"
-echo
-# Set input switching to Shift+Alt (like in Windows)
-read -r -p "By the way, do you want to toggle keyboard layouts with Shift+Alt? (default=yes)" response
-echo
-response=${response,,}
-if [[ $response =~ ^(yes|y| ) ]] | [ -z $response ]; then
-    gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Shift>Alt_L']"
-    gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Alt>Shift_L']"
-fi
-
